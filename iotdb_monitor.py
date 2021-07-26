7273b66e9968aca78b8d2beebe079ef0787e35e3
xiaoyekanren <876670773@qq.com>
2021-07-26 19:18:55 +0800

    将输出转换成int

--- iotdb_monitor.py
+++ iotdb_monitor.py
@@ -59,7 +59,7 @@ if __name__ == '__main__':
         else:
             path = cf.get('path', 'data')
         results = os.popen('find %s -name \'*.tsfile\' | xargs du -s -c | tail -n 1' % path).read()
-        print(results.split('\t')[0])
+        print(int(results.split('\t')[0]))
     elif para == 'count_unseq':
         if not cf.get('path', 'data'):
             path = os.path.join(cf.get('path', 'home'), 'data/data/unsequence')
@@ -73,7 +73,7 @@ if __name__ == '__main__':
         else:
             path = cf.get('path', 'data')
         results = os.popen('find %s -name \'*.tsfile\' | xargs du -s -c | tail -n 1' % path).read()
-        print(results.split('\t')[0])
+        print(int(results.split('\t')[0]))
     elif para == 'count_all':
         if not cf.get('path', 'data'):
             path = os.path.join(cf.get('path', 'home'), 'data/data')
@@ -87,33 +87,33 @@ if __name__ == '__main__':
         else:
             path = cf.get('path', 'data')
         results = os.popen('find %s -name \'*.tsfile\' | xargs du -s -c | tail -n 1' % path).read()
-        print(results.split('\t')[0])
+        print(int(results.split('\t')[0]))
     elif para == 'sum_resource':
         if not cf.get('path', 'data'):
             path = os.path.join(cf.get('path', 'home'), 'data/data')
         else:
             path = cf.get('path', 'data')
         results = os.popen('find %s -name \'*.tsfile.resource\' | xargs du -s -c | tail -n 1' % path).read()
-        print(results.split('\t')[0])
+        print(int(results.split('\t')[0]))
     elif para == 'sum_wal':
         if not cf.get('path', 'wal'):
             path = os.path.join(cf.get('path', 'home'), 'data/wal')
         else:
             path = cf.get('path', 'wal')
         results = os.popen('du -s %s' % path).read()
-        print(results.split('\t')[0])
+        print(int(results.split('\t')[0]))
     elif para == 'io_tps':
         disk = cf.get('system', 'disk')
         results = os.popen('iostat |grep \'%s\' |awk \'{print $2}\'' % disk).read()
-        print(results)
+        print(int(results))
     elif para == 'io_read_kb_s':
         disk = cf.get('system', 'disk')
         results = os.popen('iostat |grep \'%s\' |awk \'{print $3}\'' % disk).read()
-        print(results)
+        print(int(results))
     elif para == 'io_write_kb_s':
         disk = cf.get('system', 'disk')
         results = os.popen('iostat |grep \'%s\' |awk \'{print $4}\'' % disk).read()
-        print(results)
+        print(int(results))
     elif para == '':
         pass
     else:
